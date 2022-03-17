04264686b5e6c08a4d4703724aa21b4a30635798
CarlosAS10 <carlosas08082003@gmail.com>
2022-03-17 10:30:27 +0100

    Tarea Opcional

--- Pruebas Unitarias Calculadora/src/AssertTest.java
+++ Pruebas Unitarias Calculadora/src/AssertTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+//Ey que pasa
 public class AssertTest {
 
 	@Test
--- Pruebas Unitarias Calculadora/src/Calculadora.java
+++ Pruebas Unitarias Calculadora/src/Calculadora.java
@@ -1,5 +1,5 @@
 public class Calculadora {
-	
+//Eyey
 	private int ans;
 	
 	public Calculadora() {
